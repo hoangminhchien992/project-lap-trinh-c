71b7864d16dddf9192c6e1da3c8f8420c2553fa0
hoangminhchien992 <hoangminhchien722@gmail.com>
2024-12-31 14:22:51 +0700

    hoàng minh chiến

---
+++ project/datatypes.h
@@ -0,0 +1,10 @@
+#include<stdbool.h>
+#include<stdbool.h> 
+//lưu kiểu giữ liệu
+ typedef struct{
+	int studentId;
+	char classroomId[10];
+	char name[30];
+	char email[35];
+	char phoneNumber[15];	
+}Student;
---
+++ project/functions.h
@@ -0,0 +1,8 @@
+#include "dataTypes.h"
+//khai báo nguyên mẫu hàm
+void startProgram();
+void menuAdmin();
+void studentMenu();
+void inputStudent(Student student[], int *length);
+void printStudent(Student student[], int length);
+//void login();
